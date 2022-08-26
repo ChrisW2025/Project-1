b77df928bb61be2bd03f2feac8871a9ea6d83538
Christopher Whitaker <908892@H2WHK19UQ6NW.tld>
2022-08-26 09:04:18 -0500

    added css to sec3 and started js

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-//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
+function getData() {
+    var place = document.getElementById("place").Value;
+    var person = document.getElementById("person").Value;
+    var verb = document.getElementById("verb").Value;
+    var adjective = document.getElementById("adjective").Value;
+    var adverb = document.getElementById("adverb").Value;
+    return[]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 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
