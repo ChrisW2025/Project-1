d2be3f6b7697bfed5955920515d251e560a94a2f
Christopher Whitaker <908892@H2WHK19UQ6NW.tld>
2022-08-29 08:55:29 -0500

    finished everything for section 3

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 function getData() {
-    var place = document.getElementById("place").Value;
-    var person = document.getElementById("person").Value;
-    var verb = document.getElementById("verb").Value;
-    var adjective = document.getElementById("adjective").Value;
-    var adverb = document.getElementById("adverb").Value;
-    return[]
+    var place = document.getElementById("place").value;
+    var person = document.getElementById("person").value;
+    var verb = document.getElementById("verb").value;
+    var adjective = document.getElementById("adjective").value;
+    var adverb = document.getElementById("adverb").value;
+    
+    document.getElementById("sentence").innerHTML = sentences(verb, adverb, person,place, adjective);
+}
+
+function sentences(verb, person, place, adjective) {
+    sentenceArray = [`Today I had to ${verb} but right in the middle of it ${person} came in with this ${adjective} book and wanted to go to ${place}`, `${person} was in ${place} trying to ${verb}. However, a person left a ${adjective} shirt behind, while crying ${adverb}`, ` Its been a log time since ${person} decided to ${verb} in ${place}. What some people said was that ${person} screamed "This strawberry is ${adjective}!! We thought ${person} went crazy.`]
+
+    ranN = Math.floor(Math.random() * sentenceArray.length)
+
+    return sentenceArray[ranN];
 }
 
 
